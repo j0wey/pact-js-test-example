ed0f518b564128ea9edf43eada99b543e0f38df8
Joel Wochele <joel.wochele@atr-software.de>
2022-08-12 09:30:56 +0200

    switch from krama to jest

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,3 @@
 module.exports = {
   preset: "jest-preset-angular",
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      stringifyContentPathRegex: '\\.(html|svg)$',
-
-      tsconfig: '<rootDir>/src/tsconfig.spec.json',
-    },
-  },
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
-  transform: {
-    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
-  },
-  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
 };
